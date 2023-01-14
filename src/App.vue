ebe5dbe76f156a4bc02271b8a09dfa9e7c2edea7
Diego Lima <diegoperes142@gmail.com>
2023-01-14 20:24:18 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="UFPA Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('@/assets/brasao_ufpa.png')"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://ppgeaa.propesp.ufpa.br/index.php/br/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">PPGEAA - UFPA</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'AppApp',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/aplicativo/PaginaPrincipal.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <h2 class="display-2 font-weight-bold mb-3">
+          IWIM
+        </h2>
+
+        <p class="subheading font-weight-regular">
+          Interface Web para Identificação de Melanoma
+        </p>
+      </v-col>
+
+      <v-col cols="12">
+        <v-img
+            :src="require('@/assets/IWIM_coracao.png')"
+            class="mx-auto"
+            contain
+            width="200"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+            block
+            elevation="2"
+            color="primary"
+            @click="abrirJanelaVerificacao"
+        >VERIFICAR UMA IMAGEM</v-btn>
+      </v-col>
+
+      <v-col cols="12">
+        <v-btn
+            block
+            elevation="2"
+            color="primary"
+            @click="abrirJanelaSubimissao"
+        >SUBMETER UMA IMAGEM</v-btn>
+      </v-col>
+    </v-row>
+  <JanelaVerificacao
+    ref="JanelaVerificacao"
+  ></JanelaVerificacao>
+  <JanelaSubimissao
+    ref="JanelaSubimissao"
+  ></JanelaSubimissao>
+  </v-container>
+</template>
+
+<script>
+  import JanelaVerificacao from '@/components/JanelaVerificacao.vue';
+  import JanelaSubimissao from '@/components/JanelaSubimissao.vue';
+  export default {
+    name: 'PaginaPrincipal',
+    components:{
+      JanelaVerificacao,
+      JanelaSubimissao
+    },
+    data: () => ({
+
+    }),
+    methods:{
+      abrirJanelaVerificacao(){
+        this.$refs.JanelaVerificacao.dialog = true;
+        console.log("Abrir Janela Verificacao");
+      },
+      abrirJanelaSubimissao(){
+        this.$refs.JanelaSubimissao.dialog = true;
+        console.log("Abrir Janela Submissao");
+      },
+
+    }
+  }
+</script>
