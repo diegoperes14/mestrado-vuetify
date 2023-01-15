5d6801d0b44c2cf3e9bf5163391d95cf849a6f66
Diego Lima <diegoperes142@gmail.com>
2023-01-15 20:26:24 -0300

    Atualização do vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_DIRETORIO_RAIZ,
+  devServer: {
+    proxy: process.env.VUE_APP_API_PROXY
+  },
   transpileDependencies: [
     'vuetify'
   ]
